899f9809ea148afd4dd299bec0c002b10f574218
vdi-terminal <vdi-terminal@EDU-WSB.local>
2026-03-21 18:26:38 +0100

    Inna nazwa 2

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Kutas"
+moje_imie = "ANDREW GOLARA"
 msg = "Hello World!"
 
 @app.route('/')
